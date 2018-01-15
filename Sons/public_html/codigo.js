101c3b34ae19a7f9a4cff91fc1db06c8b4ee6e35
presidentpt <30292626+presidentpt@users.noreply.github.com>
2018-01-15 20:26:06 +0000

    updated obj and global vars

--- Sons/public_html/codigo.js
+++ Sons/public_html/codigo.js
@@ -1,10 +1,11 @@
 
 var opA, opB, opC;
 var imgA, imgB, imgC;
+var squareA, squareB, squareC;
 var animals = ["cat", "chicken", "cow", "dog", "dolphin", "donkey", "duck", "elephant", "goat", "horse", "mosquito", "seagull"];
 
 
-function Animal(img) {
+function Animal(square) {
     this.randAnimal = function(){
         var randomNum = Math.floor((Math.random() * 12)+1);
         this.name = animals[randomNum];
@@ -19,7 +20,7 @@ function Animal(img) {
     this.name = "";
     this.imgs = "";
     this.sounds = "";
-    this.imgLoc = img;
+    this.imgLoc = "img" + square;
     
     
     this.printPic = function(){
@@ -71,10 +72,6 @@ function Animal(img) {
 //};
 
  function jogar(){
-        
-        squareA = new Animal(A);
-        squareB = new Animal(B);
-        squareC = new Animal(C);
         squareA.randAnimal();
         squareB.randAnimal();
         squareC.randAnimal();
@@ -82,12 +79,17 @@ function Animal(img) {
     };
 
 function init(){
+    
     opA = document.getElementById("opA");
     opB = document.getElementById("opB");
     opC = document.getElementById("opC");
     imgA = document.getElementById("imgA");
     imgB = document.getElementById("imgB");
     imgC = document.getElementById("imgC");
+    
+    squareA = new Animal("A");
+    squareB = new Animal("B");
+    squareC = new Animal("C");
 
     jogar();
 //    animal.randAnimal();
